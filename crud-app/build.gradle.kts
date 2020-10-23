5c528354dba1215e3ea2c1827d7a9c7a31154875
Egor <ze99@rambler.ru>
2020-10-23 22:25:16 +0300

    add ktor

--- crud-app/build.gradle.kts
+++ crud-app/build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
 
     val exposedVersion = "0.26.1"
     val h2Version = "1.4.200"
+    var ktorVersion = "1.4.1"
 
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
@@ -22,7 +23,9 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
     implementation("com.h2database:h2:$h2Version")
-
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-gson:$ktorVersion")
 
 
 }
